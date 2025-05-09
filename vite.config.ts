4a533cc0a6ab2771f9ae313eda7c376969ec18aa
Miguel Angel Prado Garcia <mprado@mosbit.com>
2025-05-09 16:46:52 -0600

    arreglo de datos

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://pragmant6.github.io/farmacia-ramos/",
+  base: '/farmacia-ramos/',
   plugins: [
     react(),
     tailwindcss(),
